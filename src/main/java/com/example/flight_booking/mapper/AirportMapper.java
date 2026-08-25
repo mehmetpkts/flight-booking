d2f855c3d9c2abe8138196cd5da97c3000612833
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-08-25 15:21:20 +0300

    airport için mapper dosyası oluşturuldu ve dosya içerisine mapping kodları taşındı

---
+++ src/main/java/com/example/flight_booking/mapper/AirportMapper.java
@@ -0,0 +1,36 @@
+package com.example.flight_booking.mapper;
+
+import com.example.flight_booking.dto.Airport.AirportCreateRequestDto;
+import com.example.flight_booking.dto.Airport.AirportFilterResponseDto;
+import com.example.flight_booking.dto.Airport.AirportUpdateRequestDto;
+import com.example.flight_booking.entity.Airport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AirportMapper {
+
+  public AirportFilterResponseDto toFilterResponseDto(Airport airport) {
+    AirportFilterResponseDto dto = new AirportFilterResponseDto();
+    dto.setName(airport.getName());
+    dto.setCity(airport.getCity());
+    dto.setCountry(airport.getCountry());
+    dto.setIataCode(airport.getIataCode());
+    return dto;
+  }
+
+  public Airport toEntity(AirportCreateRequestDto createRequest) {
+    Airport airport = new Airport();
+    airport.setName(createRequest.getName());
+    airport.setCountry(createRequest.getCountry());
+    airport.setCity(createRequest.getCity());
+    airport.setIataCode(createRequest.getIataCode());
+    return airport;
+  }
+
+  public void updateEntity(Airport airport, AirportUpdateRequestDto updateRequest) {
+    airport.setName(updateRequest.getName());
+    airport.setCity(updateRequest.getCity());
+    airport.setCountry(updateRequest.getCountry());
+    airport.setIataCode(updateRequest.getIataCode());
+  }
+}
--- src/main/java/com/example/flight_booking/service/AirportService.java
+++ src/main/java/com/example/flight_booking/service/AirportService.java
@@ -4,6 +4,7 @@ import com.example.flight_booking.dto.Airport.AirportCreateRequestDto;
 import com.example.flight_booking.dto.Airport.AirportFilterResponseDto;
 import com.example.flight_booking.dto.Airport.AirportUpdateRequestDto;
 import com.example.flight_booking.entity.Airport;
+import com.example.flight_booking.mapper.AirportMapper;
 import com.example.flight_booking.repository.AirportRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ import org.springframework.web.server.ResponseStatusException;
 public class AirportService {
 
   private final AirportRepository airportRepository;
+  private final AirportMapper airportMapper;
 
-  public AirportService(AirportRepository airportRepository) {
+  public AirportService(AirportRepository airportRepository, AirportMapper airportMapper) {
     this.airportRepository = airportRepository;
+    this.airportMapper = airportMapper;
   }
 
   public Airport getAirportEntityById(Long id) {
@@ -26,35 +29,18 @@ public class AirportService {
 
   public AirportFilterResponseDto getAirportById(Long id){
     Airport airport = getAirportEntityById(id);
-
-    AirportFilterResponseDto dto = new AirportFilterResponseDto();
-    dto.setName(airport.getName());
-    dto.setCity(airport.getCity());
-    dto.setCountry(airport.getCountry());
-    dto.setIataCode(airport.getIataCode());
-    return dto;
+    return airportMapper.toFilterResponseDto(airport);
   }
 
   public Airport createAirport(AirportCreateRequestDto create) {
-    Airport airport = new Airport();
-
-    airport.setName(create.getName());
-    airport.setCountry(create.getCountry());
-    airport.setCity(create.getCity());
-    airport.setIataCode(create.getIataCode());
-
+    Airport airport = airportMapper.toEntity(create);
     return airportRepository.save(airport);
   }
 
 
   public Airport updateAirport(Long id, AirportUpdateRequestDto update) {
     Airport airport = getAirportEntityById(id);
-
-    airport.setName(update.getName());
-    airport.setCity(update.getCity());
-    airport.setCountry(update.getCountry());
-    airport.setIataCode(update.getIataCode());
-
+    airportMapper.updateEntity(airport, update);
     return airportRepository.save(airport);
   }
 
