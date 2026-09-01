f4cf2f6a9efc4905505890ed9e0a459807f99ee8
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-01 09:40:13 +0300

    crewAssignmentController için logger altyapısı eklendi

--- src/main/java/com/example/flight_booking/controller/CrewAssignmentController.java
+++ src/main/java/com/example/flight_booking/controller/CrewAssignmentController.java
@@ -6,6 +6,8 @@ import com.example.flight_booking.dto.CrewAssignment.CrewAssignmentUpdateRequest
 import com.example.flight_booking.entity.CrewAssignment;
 import com.example.flight_booking.service.CrewAssignmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,28 +29,44 @@ public class CrewAssignmentController {
     this.crewAssignmentService = crewAssignmentService;
   }
 
+  private static final Logger logger = LoggerFactory.getLogger(CrewAssignmentController.class);
+
   @GetMapping("/{id}")
   public ResponseEntity<CrewAssignmentFilterResponseDto> getCrewAssignmentById(@PathVariable Long id) {
-    return ResponseEntity.ok(crewAssignmentService.getCrewAssignmentById(id));
+
+    logger.info("Ekip atam tablosundan id'ye göre veri çekme isteği oluşturuldu.");
+    CrewAssignmentFilterResponseDto crewAssignmnet = crewAssignmentService.getCrewAssignmentById(id);
+    logger.info("Ekip atam tablosundan id'ye göre veri çekme isteği çekildi.");
+
+    return ResponseEntity.ok(crewAssignmnet);
   }
 
   @PostMapping
   public ResponseEntity<CrewAssignment> createCrewAssignment(
       @Valid @RequestBody CrewAssignmentCreateRequestDto create) {
+
+    logger.info("Ekip atama tablosuna yeni veri ekleme isteği oluşturuldu.");
     CrewAssignment crewAssignment = crewAssignmentService.createCrewAssignment(create);
+    logger.info("Ekip atama tablosuna yeni veri eklendi.");
+
     return ResponseEntity.status(HttpStatus.CREATED).body(crewAssignment);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<CrewAssignment> updateCrewAssignment(@PathVariable Long id,
       @Valid @RequestBody CrewAssignmentUpdateRequestDto update) {
+
+    logger.info("id'ye göre görev atama kısmında güncelleme isteği alındı.");
     CrewAssignment crewAssignment = crewAssignmentService.updateCrewAssignment(id, update);
+    logger.info("id'ye göre görev atama kısmında güncelleme gerçekleşti!");
     return ResponseEntity.ok(crewAssignment);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteCrewAssignment(@PathVariable Long id) {
+    logger.info("id'ye görev atama silinme isteği alındı.");
     crewAssignmentService.deleteCrewAssignment(id);
+    logger.info("id'ye göre görev atama silindi!");
     return ResponseEntity.noContent().build();
   }
 
