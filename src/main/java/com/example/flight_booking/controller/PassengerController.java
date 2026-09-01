b5cf685e546b9b34224c8228a7c52a26b86e4cb4
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-01 10:37:41 +0300

    Passenger için logger altyapısı eklendi

--- src/main/java/com/example/flight_booking/controller/PassengerController.java
+++ src/main/java/com/example/flight_booking/controller/PassengerController.java
@@ -6,6 +6,8 @@ import com.example.flight_booking.dto.Passenger.PassengerUpdateRequestDto;
 import com.example.flight_booking.entity.Passenger;
 import com.example.flight_booking.service.PassengerService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,28 +28,45 @@ public class PassengerController {
   public PassengerController(PassengerService passengerService) {
     this.passengerService = passengerService;
   }
+  private static final Logger logger = LoggerFactory.getLogger(PassengerController.class);
 
   @GetMapping("/{id}")
   public ResponseEntity<PassengerFilterResponseDto> getPassengerById(@PathVariable Long id) {
-    return ResponseEntity.ok(passengerService.getPassengerById(id));
+
+    logger.info("id'ye göre yolcu isteği alındı.");
+    PassengerFilterResponseDto passenger = passengerService.getPassengerById(id);
+    logger.info("id'ye yolcu getirildi.");
+    return ResponseEntity.ok(passenger);
   }
 
   @PostMapping
   public ResponseEntity<Passenger> createPassenger(
       @Valid @RequestBody PassengerCreateRequestDto create) {
+
+    logger.info("Yolcu oluşturma isteği alındı.");
     Passenger savedPassenger = passengerService.createPassenger(create);
+    logger.info("Yolcu oluşturuldu.");
+
     return ResponseEntity.status(HttpStatus.CREATED).body(savedPassenger);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Passenger> updatePassenger(@PathVariable Long id, @Valid @RequestBody PassengerUpdateRequestDto update) {
+
+    logger.info("Yolcu özellikeleri değiştirme isteği alındı.");
     Passenger updatedPassenger = passengerService.updatePassenger(id, update);
+    logger.info("Yolcu özelliği-leri değiştirildi.");
+
     return ResponseEntity.ok(updatedPassenger);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deletePassenger(@PathVariable Long id) {
+
+    logger.info("Yolcu silinme isteği alındı.");
     passengerService.deletePassenger(id);
+    logger.info("Yolcu silindi!");
+
     return ResponseEntity.noContent().build();
   }
 
