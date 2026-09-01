4ec72dc5ac34525fa17d814bfa17a4d1fd33c084
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-01 09:27:10 +0300

    bookingcontroller için logger altyapısı eklendi

--- src/main/java/com/example/flight_booking/controller/BookingController.java
+++ src/main/java/com/example/flight_booking/controller/BookingController.java
@@ -6,6 +6,8 @@ import com.example.flight_booking.dto.Booking.BookingUpdateRequestDto;
 import com.example.flight_booking.entity.Booking;
 import com.example.flight_booking.service.BookingService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,29 +28,46 @@ public class BookingController {
   public BookingController(BookingService bookingService) {
     this.bookingService = bookingService;
   }
+  private static final Logger logger = LoggerFactory.getLogger(BookingController.class);
 
   @GetMapping("/{id}")
   public ResponseEntity<BookingFilterResponseDto> getBookingById(@PathVariable Long id) {
-    return ResponseEntity.ok(bookingService.getBookingById(id));
+
+    logger.info("Id ile rezervasyon getirilme isteği oluşturuldu:");
+    BookingFilterResponseDto booking = bookingService.getBookingById(id);
+    logger.info("Rezervasyon getirildi.");
+
+    return ResponseEntity.ok(booking);
   }
 
   @PostMapping
   public ResponseEntity<Booking> createBooking(@Valid @RequestBody BookingCreateRequestDto create) {
+
+    logger.info("Rezervasyon güncelleme isteği alındı.");
     Booking savedBooking = bookingService.createBooking(create);
+    logger.info("Rezervasyon güncellendi!");
+
     return ResponseEntity.status(HttpStatus.CREATED).body(savedBooking);
   }
 
   @PutMapping("/{id}/status")
   public ResponseEntity<Booking> updateBookingStatus(@PathVariable Long id,
       @Valid @RequestBody BookingUpdateRequestDto update) {
+
+    logger.info("Rezevasyon oluşturma isteği alındı");
     Booking updatedBooking = bookingService.updateBookingStatus(id, update);
+    logger.info("Rezervasyon oluşturuldu!");
     return ResponseEntity.ok(updatedBooking);
   }
 
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteBooking(@PathVariable Long id) {
+
+    logger.info("Rezervasyon silme isteği oluşturuldu.");
     bookingService.deleteBooking(id);
+    logger.info("Rezervasyon silindi.");
+
     return ResponseEntity.noContent().build();
   }
 
