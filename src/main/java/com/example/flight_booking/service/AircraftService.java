dd54fb1ba3021697f5649d731e869b075ea5b2e2
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-02 22:02:36 +0300

    aircraft service için log yapısı oluşturuldu

--- src/main/java/com/example/flight_booking/service/AircraftService.java
+++ src/main/java/com/example/flight_booking/service/AircraftService.java
@@ -7,6 +7,8 @@ import com.example.flight_booking.entity.Aircraft;
 import com.example.flight_booking.entity.Airline;
 import com.example.flight_booking.mapper.AircraftMapper;
 import com.example.flight_booking.repository.AircraftRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,6 +19,7 @@ public class AircraftService {
   private final AircraftRepository aircraftRepository;
   private final AirlineService airlineService;
   private final AircraftMapper aircraftMapper;
+  private static final Logger logger = LoggerFactory.getLogger(AircraftService.class);
 
   public AircraftService(
       AircraftRepository aircraftRepository,
@@ -27,10 +30,15 @@ public class AircraftService {
     this.aircraftMapper = aircraftMapper;
   }
 
+  // bu kod yapısı exception dosyası oluşturulduktan sonra daha sade bir biçimde yazılacak.
   public Aircraft getAircraftEntityById(Long id){
-    return aircraftRepository
-            .findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus
-                    .NOT_FOUND, "aircraft id is not: " + id));
+    logger.debug("Uçak aranıyor! aircraftId = {}", id);
+    return aircraftRepository.findById(id).orElseThrow(() ->{
+      logger.warn("Uçak bulunamadı aircraftId: {}", id);
+      return new ResponseStatusException(
+              HttpStatus.NOT_FOUND, "aircraf id is not: " + id
+      );
+    });
   }
 
   private Airline getAirlineEntityById(Long id) {
@@ -43,21 +51,37 @@ public class AircraftService {
   }
 
   public Aircraft createAircraft(AircraftCreateRequestDto create) {
+    logger.info("Aircraft oluşturuluyor. airlineId={}", create.getAirlineId());
+
     Airline airline = getAirlineEntityById(create.getAirlineId());
     Aircraft aircraft = aircraftMapper.toEntity(create, airline);
-    return aircraftRepository.save(aircraft);
+    Aircraft savedAircraft = aircraftRepository.save(aircraft);
+
+    logger.info("Aircraft oluşturuldu. aircraftId={}, airlineId={}",
+        savedAircraft.getAircraftId(), airline.getAirlineId());
+    return savedAircraft;
   }
 
 
   public Aircraft updateAircraft(Long id, AircraftUpdateRequestDto update) {
+    logger.info("Aircraft güncelleniyor. aircraftId={}, airlineId={}",
+        id, update.getAirlineId());
+
     Airline airline = getAirlineEntityById(update.getAirlineId());
     Aircraft aircraft = getAircraftEntityById(id);
     aircraftMapper.updateEntity(aircraft, update, airline);
-    return aircraftRepository.save(aircraft);
+    Aircraft savedAircraft = aircraftRepository.save(aircraft);
+
+    logger.info("Aircraft güncellendi. aircraftId={}", savedAircraft.getAircraftId());
+    return savedAircraft;
   }
 
   public void deleteAircraft(Long id) {
+    logger.info("Aircraft siliniyor. aircraftId={}", id);
+
     aircraftRepository.delete(getAircraftEntityById(id));
+
+    logger.info("Aircraft silindi. aircraftId={}", id);
   }
 
 }
