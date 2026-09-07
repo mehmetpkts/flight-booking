0fed3bb262885906247a65d47dbaeecfa4720d0e
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-07 10:24:40 +0300

    airport service için log yapısı eklendi

--- src/main/java/com/example/flight_booking/service/AirportService.java
+++ src/main/java/com/example/flight_booking/service/AirportService.java
@@ -5,6 +5,8 @@ import com.example.flight_booking.dto.Airport.AirportFilterResponseDto;
 import com.example.flight_booking.entity.Airport;
 import com.example.flight_booking.mapper.AirportMapper;
 import com.example.flight_booking.repository.AirportRepository;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,6 +16,7 @@ public class AirportService {
 
   private final AirportRepository airportRepository;
   private final AirportMapper airportMapper;
+  private static final Logger logger = LoggerFactory.getLogger(AirportService.class);
 
   public AirportService(AirportRepository airportRepository, AirportMapper airportMapper) {
     this.airportRepository = airportRepository;
@@ -21,9 +24,12 @@ public class AirportService {
   }
 
   public Airport getAirportEntityById(Long id) {
-    return airportRepository.findById(id)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-            "Airport not found with id " + id));
+    logger.debug("Havalimanı aranıyor! AirportId: {}", id);
+    return airportRepository.findById(id).orElseThrow(() -> {logger.warn("Havalimanı bulunamadı, AirportId: {}",id);
+      return new ResponseStatusException(
+              HttpStatus.NOT_FOUND, "Airline id is not: "+ id
+      );
+    });
   }
 
   public AirportFilterResponseDto getAirportById(Long id){
@@ -32,13 +38,24 @@ public class AirportService {
   }
 
   public Airport createAirport(AirportCreateRequestDto create) {
+    logger.info("Airport oluşturuluyor. name={}, iataCode={}",
+        create.getName(), create.getIataCode());
+
     Airport airport = airportMapper.toEntity(create);
-    return airportRepository.save(airport);
+    Airport savedAirport = airportRepository.save(airport);
+
+    logger.info("Airport oluşturuldu. airportId={}, iataCode={}",
+        savedAirport.getAirportId(), savedAirport.getIataCode());
+    return savedAirport;
   }
 
   public void deleteAirport(Long id) {
+    logger.info("Airport siliniyor. airportId={}", id);
+
     Airport airport = getAirportEntityById(id);
     airportRepository.delete(airport);
+
+    logger.info("Airport silindi. airportId={}", id);
   }
 
 }
