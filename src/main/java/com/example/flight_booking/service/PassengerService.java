1220e1f1427d1381739021f7c83ea36458842076
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2026-09-07 10:46:47 +0300

    passenger service için loglama yapısı oluşturuldu

--- src/main/java/com/example/flight_booking/service/PassengerService.java
+++ src/main/java/com/example/flight_booking/service/PassengerService.java
@@ -6,6 +6,8 @@ import com.example.flight_booking.dto.Passenger.PassengerUpdateRequestDto;
 import com.example.flight_booking.entity.Passenger;
 import com.example.flight_booking.mapper.PassengerMapper;
 import com.example.flight_booking.repository.PassengerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class PassengerService {
 
+  private static final Logger logger = LoggerFactory.getLogger(PassengerService.class);
   private final PassengerRepository passengerRepository;
   private final PassengerMapper passengerMapper;
 
@@ -22,9 +25,13 @@ public class PassengerService {
   }
 
   public Passenger getPassengerEntityById(Long id) {
+    logger.debug("Yolcu aranıyor. passengerId={}", id);
     return passengerRepository.findById(id)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-            "Passenger not defined. Id is: " + id));
+        .orElseThrow(() -> {
+          logger.warn("Yolcu bulunamadı. passengerId={}", id);
+          return new ResponseStatusException(HttpStatus.NOT_FOUND,
+              "Passenger not defined. Id is: " + id);
+        });
   }
 
   public PassengerFilterResponseDto getPassengerById(Long id) {
@@ -33,17 +40,31 @@ public class PassengerService {
   }
 
   public Passenger createPassenger(PassengerCreateRequestDto create) {
+    logger.info("Yolcu oluşturuluyor.");
+
     Passenger passenger = passengerMapper.toEntity(create);
-    return passengerRepository.save(passenger);
+    Passenger savedPassenger = passengerRepository.save(passenger);
+
+    logger.info("Yolcu oluşturuldu. passengerId={}", savedPassenger.getPassengerId());
+    return savedPassenger;
   }
 
   public Passenger updatePassenger(Long id, PassengerUpdateRequestDto update) {
+    logger.info("Yolcu güncelleniyor. passengerId={}", id);
+
     Passenger passenger = getPassengerEntityById(id);
     passengerMapper.updateEntity(passenger, update);
-    return passengerRepository.save(passenger);
+    Passenger savedPassenger = passengerRepository.save(passenger);
+
+    logger.info("Yolcu güncellendi. passengerId={}", savedPassenger.getPassengerId());
+    return savedPassenger;
   }
 
   public void deletePassenger(Long id) {
+    logger.info("Yolcu siliniyor. passengerId={}", id);
+
     passengerRepository.delete(getPassengerEntityById(id));
+
+    logger.info("Yolcu silindi. passengerId={}", id);
   }
 }
